85c7a4d3322fffda594db952d489934ec1f0707e
kathrynzaro <zarokathryn@gmail.com>
2022-09-15 16:00:02 -0700

    adds get(id)

--- __tests__/simple-db.test.js
+++ __tests__/simple-db.test.js
@@ -6,14 +6,10 @@ const BASE_DIR = CI ? HOME : __dirname;
 const TEST_DIR = path.join(BASE_DIR, 'test-dir');
 
 describe('simple database', () => {
-
   beforeEach(async () => {
     await fs.rm(TEST_DIR, { force: true, recursive: true });
     await fs.mkdir(TEST_DIR, { recursive: true });
   });
 
-  it('needs a first test...', async () => {
-
-  });
-
+  it('gets file by id', async () => {});
 });
