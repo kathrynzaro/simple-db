ed3465dfd58dab46635d4f92f35ead63bcb7ddec
kathrynzaro <zarokathryn@gmail.com>
2022-09-15 17:37:38 -0700

    adds save(obj), tests for get(id) and save

--- __tests__/simple-db.test.js
+++ __tests__/simple-db.test.js
@@ -1,5 +1,7 @@
-const fs = require('fs/promises');
+const fs = require('node:fs/promises');
 const path = require('path');
+const SimpleDb = require('../lib/simple-db');
+const crypto = require('crypto');
 
 const { CI, HOME } = process.env;
 const BASE_DIR = CI ? HOME : __dirname;
@@ -11,5 +13,24 @@ describe('simple database', () => {
     await fs.mkdir(TEST_DIR, { recursive: true });
   });
 
-  it('gets file by id', async () => {});
+  it('gets file by id', async () => {
+    const dinner = {
+      name: 'burrito',
+    };
+    const id = crypto.randomBytes(8).toString('hex');
+    await fs.writeFile(`${TEST_DIR}/${id}.json`, JSON.stringify(dinner));
+    const db = new SimpleDb(TEST_DIR);
+    const result = await db.getFileById(id);
+    expect(result).toEqual(dinner);
+  });
+
+  it('saves', async () => {
+    const dinner = {
+      name: 'breakfast',
+    };
+    const db = new SimpleDb(TEST_DIR);
+    await db.save(dinner);
+    const res = await db.getFileById(dinner.id);
+    expect(res).toEqual(dinner);
+  });
 });
